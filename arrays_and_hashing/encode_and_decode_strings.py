da2024e06f01dca7d7486fa2b705c9b93fb56779
Pablo Salinas <salinas.pablom@gmail.com>
2024-11-26 11:27:58 -0300

    Fix a horrible scenario where the length of an encoded number is greater than 1

--- arrays_and_hashing/encode_and_decode_strings.py
+++ arrays_and_hashing/encode_and_decode_strings.py
@@ -8,19 +8,23 @@ Please implement encode and decode
 '''
 from typing import List
 
+
 class Solution:
-
     def encode(self, strs: List[str]) -> str:
-        encoded = [f'{len(string)}{string}' for string in strs]
-        return ''.join(encoded)
-
+        return ''.join((f'{len(s)}|{s}' for s in strs))
+ 
     def decode(self, s: str) -> List[str]:
-        l = []
-        i = 0
+        result = []
+        current_index = 0
         N = len(s)
-        while i < N:
-            length = int(s[i])
-            string = s[i+1:i+length+1]
-            l.append(string)
-            i += length + 1
-        return l
+        while current_index < N:
+            # first, parse the length of the next word
+            next_word_length = 1
+            while s[current_index+next_word_length] != '|':
+                next_word_length += 1
+            length = int(s[current_index:current_index+next_word_length])
+            next_string = s[current_index+next_word_length+1:current_index+next_word_length+1+length]
+            result.append(next_string)
+            current_index += length + next_word_length + 1
+ 
+        return result
--- arrays_and_hashing/test_encode_and_decode_strings.py
+++ arrays_and_hashing/test_encode_and_decode_strings.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from typing import List
 from .encode_and_decode_strings import Solution
 
 
@@ -10,6 +11,9 @@ class TestEncodeAndDecodeStrings:
     def test_two(self):
         self._assert_decode_encode_list_returns_original_list(["we","say",":","yes"])
 
-    def _assert_decode_encode_list_returns_original_list(self, l):
+    def test_encode_with_long_strins(self):
+        self._assert_decode_encode_list_returns_original_list(["we","say",":","yes","!@#$%^&*()"])
+
+    def _assert_decode_encode_list_returns_original_list(self, l: List[str]):
         s = Solution()
         assert s.decode(s.encode(l)) == l
